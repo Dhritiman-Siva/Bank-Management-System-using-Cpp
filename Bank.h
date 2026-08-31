3567e1a0fc3e1bc54a7917386110b79ef6b975b7
Dhritiman Siva <dhritimansiva2@gmail.com>
2026-08-31 23:35:25 +0530

    defining Bank.h

---
+++ Bank.h
@@ -0,0 +1,20 @@
+#ifndef BANK_H
+#define BANK_H
+
+#include "BankAccount.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Bank
+{
+private:
+    vector<BankAccount> accounts;
+    string filename;
+
+public:
+
+};
+
+#endif
